f97136139fe737017d2249036a7f69b8ba003986
hugo.frauches <hugo.frauches@gmail.com>
2015-01-22 22:40:53 -0200

    Gera Tile

---
+++ Devil Jump/Assets/Script/GeraTile.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeraTile : MonoBehaviour {
+
+
+	public GameObject[] tiles = new GameObject[4];
+	public float tileDistance = 0.7f;
+
+	int xLength;
+	int yLength;
+
+	int[,] tileMap = {
+		{1,1,1,1,1,1,1,1,1,1},
+		{2,0,0,0,0,0,0,0,0,2},
+		{2,0,0,0,0,0,0,0,0,2},
+		{2,0,0,0,0,0,0,0,0,2},
+		{2,0,0,0,0,0,0,0,0,2},
+		{2,0,0,0,0,0,0,0,0,2},
+		{2,0,0,0,0,0,0,0,0,2},
+		{2,0,0,0,0,0,0,0,0,2},
+		{2,0,0,0,0,0,0,0,0,2},
+		{1,1,1,1,1,1,1,1,1,1},
+	};
+
+	void Start()
+	{
+		xLength = tileMap.GetLength (0);
+		yLength = tileMap.GetLength (1);
+
+		GeraMap ();
+	}
+
+	public void GeraMap()
+	{
+		for (int i = xLength - 1, y = 0; i >= 0; i--, y++) 
+		{
+			for (int j = 0, x = 0; j < yLength; j++, x++) 
+			{
+				if(tileMap[i,j] != 0)
+				{
+					GameObject tile = Instantiate(tiles[tileMap[i,j]],new Vector2(x * tileDistance,y * tileDistance), transform.rotation) as GameObject;
+					tile.transform.parent = transform;
+				}
+			}
+		}
+	}
+}
