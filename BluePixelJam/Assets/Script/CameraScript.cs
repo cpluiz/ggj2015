0b7566ef3841e6d7c8568ed1d9aab120823b045e
Luiz Fernando de Lira <luizflira@gmail.com>
2015-01-24 19:25:11 -0200

    Mais uma versão que não funciona pra poder dar merge

--- BluePixelJam/Assets/Script/CameraScript.cs
+++ BluePixelJam/Assets/Script/CameraScript.cs
@@ -16,12 +16,13 @@ public class CameraScript : MonoBehaviour
     }
 
     public void setBounds(Bounds mapBounds, Transform geraTile) {
+        GameObject.FindWithTag("PlayerManager").GetComponent<PlayerManager>().forceStart();
         float vertExtent = Camera.main.camera.orthographicSize;
         float horzExtent = vertExtent * Screen.width / Screen.height;
-        Vector3 point = camera.WorldToViewportPoint(geraTile.position);
-        Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
-        Vector3 limitCenter = new Vector3(mapBounds.extents.x/2, mapBounds.extents.y/2);
-        mapBounds.center = point + delta + limitCenter;
+        Vector3 limitCenter = new Vector3(mapBounds.extents.x, mapBounds.extents.y);
+        Vector3 point = camera.WorldToViewportPoint(geraTile.position+limitCenter);
+        Vector3 delta = geraTile.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+        mapBounds.center = point + delta;
         leftBound = (float)(horzExtent - mapBounds.size.x / 2.0f);
         rightBound = (float)(mapBounds.size.x / 2.0f - horzExtent);
         bottomBound = (float)(vertExtent - mapBounds.size.y / 2.0f);
