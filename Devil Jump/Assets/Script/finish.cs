8bc618a250f51f1a6f5424d409378985c414ecb7
Rodrigo Barreto <miragebr@bibucket.org>
2015-01-24 01:20:41 -0200

    animação dos botões

---
+++ Devil Jump/Assets/Script/finish.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class finish : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision col){
+		Debug.Log ("AE");
+
+		if(col.gameObject.name == "Player"){
+			Application.LoadLevel(3);
+		}
+	}
+}
